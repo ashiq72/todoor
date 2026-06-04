b02854188f84a1d9d19ea766942108181c3ed6f7
Ashiq Ahmed <ahmedashik18k@gmail.com>
2026-06-04 18:29:38 +0600

    Updated

---
+++ backend/src/config/index.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+
+export default {
+  port: process.env.PORT,
+//   NODE_ENV: process.env.NODE_ENV,
+//   database_url: process.env.DATABASE_URL,
+//   default_password: process.env.DEFAULT_PASS,
+//   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+//   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+//   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+//   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN || "1d",
+//   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN || "365d",
+//   reset_pass_ui_link: process.env.RESET_PASS_UI_LINK,
+//   cloudinary: {
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+//   },
+//   upload: {
+//     max_file_size_mb: Number(process.env.UPLOAD_MAX_MB || "5"),
+//     allowed_mime: (process.env.UPLOAD_ALLOWED_MIME || "image/jpeg,image/png").split(
+//       ",",
+//     ),
+//   },
+};
